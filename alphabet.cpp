c485506c1581b19bab6a6288350d43859a9fd1d6
JeraldSanInterno <qjsminterno@tip.edu.ph>
2024-09-25 12:35:29 +0800

    Add files via upload

---
+++ alphabet.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//create a c++ program to determine if alphabet is vowel or consonant 
+	
+	char letters;
+	
+	cout << "Enter the character: ";
+	cin >> letters;
+	
+	switch(letters)
+	{
+		case 'A':
+		case 'a':
+			cout << "The letter is a vowel";
+		break; 
+		
+		case 'E':
+		case 'e':
+			cout << "The letter is a vowel";
+		break;
+		
+		case 'I':
+		case 'i':
+			cout << "The letter is a vowel";
+		break;
+		
+		case 'O':
+		case 'o':
+			cout << "The letter is a vowel";
+		break;
+		
+		case 'U':
+		case 'u':	
+			cout << "The letter is a vowel";
+		break; 
+		
+		default:
+			{
+				cout <<"The letter is a consonant";
+				break;
+			}
+	}
+	
+ return 0;
+}
